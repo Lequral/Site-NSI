e2fdf1ef36d77130b028611844a9c2c83a479d0e
Lequral <monsieur.lol@yahoo.com>
2021-11-03 18:22:32 +0100

    Changements animation
    Mtn on met la class "animate" au éléments qu'on veut animer et faut aussi ajouter dans yMinMax, une liste pour les % où l'animation s'activera (ex: la première s'active quand on a scroll de 45% à 90%.)

--- accueil.js
+++ accueil.js
@@ -1,14 +1,46 @@
-const tryAnimate = () => {
-    if (pageYpourcent > 45) {
-        document.getElementById("resume").style.animationPlayState = "running";
-    }
-    return pageYpourcent
+var toAnimate = document.getElementsByClassName("animate")
+
+var pageYpourcent;
+
+const restartAnimation = i => {
+    toAnimate[i].classList.remove("animate")
+    void toAnimate[i].offsetWidth;
+    toAnimate[i].classList.add("animate")
 }
 
-var pageYpourcent = Math.round(scrollY / scrollMaxY * 100)
-console.log(tryAnimate())
+const tryAnimate = () => {
+
+    let yMinMax = [
+        [45, 90],
+    ]
+
+    for (i in [Array(toAnimate.length)]) {
+
+        let yCoord = yMinMax[i]
+        
+        console.log(yCoord[0] < pageYpourcent && pageYpourcent < yCoord[1])
+        if (yCoord[0] < pageYpourcent && pageYpourcent < yCoord[1]) {
+            toAnimate[i].style.animationPlayState = "running";
+            console.log("animate")
+        }else{
+            console.log(i)
+            let jsp = toAnimate[i];
+            if(jsp.style.animationPlayState == "running") {
+                restartAnimation(i)
+                console.log("restart animation")
+            }
+        }
+    }
+    return pageYpourcent + "%"
+}
+
+
+window.onload = function () {
+    pageYpourcent = Math.round(scrollY / (document.body.scrollHeight - innerHeight) * 100);
+    console.log(tryAnimate())
+}
 
 window.addEventListener('scroll', function () {
-    pageYpourcent = Math.round(scrollY / scrollMaxY * 100)
+    pageYpourcent = Math.round(scrollY / (document.body.scrollHeight - innerHeight) * 100);
     console.log(tryAnimate())
 });
