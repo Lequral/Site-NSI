277b75076291de9c2fa4d7f9485aa2018c4bf0c9
Lequral <monsieur.lol@yahoo.com>
2021-11-03 19:38:20 +0100

    Animations
    Mtn l'animation peuvent se désactivé qd on ne voit plus l'élément aussi le code marchera qd on ajoutera d'autres blocs à animer.

--- accueil.js
+++ accueil.js
@@ -3,31 +3,36 @@ var toAnimate = document.getElementsByClassName("animate")
 var pageYpourcent;
 
 const restartAnimation = i => {
-    toAnimate[i].classList.remove("animate")
-    void toAnimate[i].offsetWidth;
-    toAnimate[i].classList.add("animate")
+
+    let element = toAnimate[i]
+    
+    element.classList.remove("animate"); 
+
+    // trigger a DOM reflow 
+    void element.offsetWidth; 
+
+    element.classList.add("animate");
+    element.style.animationPlayState = "paused"
 }
 
 const tryAnimate = () => {
 
     let yMinMax = [
-        [45, 90],
+        [20, 100],
     ]
 
     for (i in [Array(toAnimate.length)]) {
 
         let yCoord = yMinMax[i]
-        
-        console.log(yCoord[0] < pageYpourcent && pageYpourcent < yCoord[1])
+
+        console.log(yCoord[0] < pageYpourcent && pageYpourcent < yCoord[1])//yCoord[0] = l'ordonné minimale pour animer, yCoord[1] = l'ordonné max
         if (yCoord[0] < pageYpourcent && pageYpourcent < yCoord[1]) {
             toAnimate[i].style.animationPlayState = "running";
             console.log("animate")
-        }else{
-            console.log(i)
-            let jsp = toAnimate[i];
-            if(jsp.style.animationPlayState == "running") {
-                restartAnimation(i)
+        } else {
+            if (toAnimate[0].style.animationPlayState == "running") {
                 console.log("restart animation")
+                restartAnimation(i)
             }
         }
     }
