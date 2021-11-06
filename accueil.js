26288b79bf5b02e4bcf780e6d1960306190590dd
Lequral <monsieur.lol@yahoo.com>
2021-11-06 23:48:05 +0100

    jsp
    je tente d trucs

--- accueil.js
+++ accueil.js
@@ -2,46 +2,67 @@ var toAnimate = document.getElementsByClassName("animate")
 
 var pageYpourcent;
 
+/**
+ * @param { HTMLDivElement } element
+*/
+
+const FadeInUp = element => {
+
+    console.log("anim")
+    element.style.opacity += 0.05
+    element.style.transform -= 0.05; //jsp
+
+}
+
+const FadeOutUp = element => {
+    console.log("fadeOutUp")
+}
+
 const restartAnimation = i => {
 
     let element = toAnimate[i]
-    
-    element.classList.remove("animate"); 
+
+    element.classList.remove("animate");
 
     // trigger a DOM reflow 
-    void element.offsetWidth; 
+    void element.offsetWidth;
 
     element.classList.add("animate");
     element.style.animationPlayState = "paused"
 }
 
-const tryAnimate = () => {
+const tryAnimate = async () => {
 
     let yMinMax = [
-        [0, 100],
-        [0, 100],
-        [0, 100],
-        [0, 100],
-        [0, 100],
-        [0, 100],
-        [0, 100],
-        [0, 100],
-        [0, 100]
+        [0, 99],
+        [0, 99],
+        [0, 99],
+        [0, 99],
+        [0, 99],
+        [0, 99],
+        [0, 99],
+        [0, 99],
+        [0, 99]
     ]
 
     for (i in [...Array(toAnimate.length).keys()]) {
         let yCoord = yMinMax[i]
-        // console.log(`i = ${i}`) pour débugger
-        
-        if (yCoord[0] < pageYpourcent && pageYpourcent < yCoord[1]) {//yCoord[0] = l'ordonné minimale pour animer, yCoord[1] = l'ordonné max
-            toAnimate[i].style.animationPlayState = "running";
-            // console.log("animate")
-        } else {
-            if (toAnimate[0].style.animationPlayState == "running") {
-                // console.log("restart animation")
-                restartAnimation(i)
-            }
-        }
+
+        if(i != 0) return
+
+        if (yCoord[0] < pageYpourcent && pageYpourcent < yCoord[1]) {
+
+            let int = 1
+            let Interval;
+
+            Interval = setInterval(FadeInUp(toAnimate[i]), 1000)
+            setTimeout(clearInterval(Interval),20 * 1000)
+
+        } //else  if (toAnimate[i].style.animationPlayState == "running") {
+
+        //         FadeOutUp(toAnimate[i])
+
+        // }
     }
     return pageYpourcent + "%"
 }
