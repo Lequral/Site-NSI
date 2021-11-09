76a5c8eb95a9934358bece0bb68fb2772bf87624
Lequral <monsieur.lol@yahoo.com>
2021-11-09 21:37:33 +0100

    Version clé usb
    Normalement j'ai bien tout mis à jour.

--- animation.js
+++ animation.js
@@ -2,11 +2,9 @@ var toAnimate = document.getElementsByClassName("animate")
 
 var pageYpourcent;
 
-const tryAnimate = () => {
+var yMinMax;
 
-    let yMinMax = [
-        [15, 100]
-    ]
+const tryAnimate = () => {
 
     for (i in [...Array(toAnimate.length).keys()]) {
 
@@ -15,9 +13,9 @@ const tryAnimate = () => {
         let element = toAnimate[i]
         let animation = element.getAnimations()[0]
 
+        if(animation === undefined) return
+ 
         if (yCoordoonates[0] < pageYpourcent && pageYpourcent < yCoordoonates[1]) {
-
-            if(animation === undefined) return
 
             if (animation.currentTime < 100 || 1900 < animation.currentTime) { //test si l'element est bien au début de l'animation donc caché
                 if(!(animation.playState === "paused")) return
@@ -53,6 +51,31 @@ const tryAnimate = () => {
 
 
 window.onload = function () {
+
+    if (document.title === "Accueil | Avatar") {
+        yMinMax = [
+            [12, 28],
+            [30, 48],
+            [48, 69],
+            [65, 80],
+            [80, 101]
+        ]
+
+        console.log("Data for Accueil page loaded")
+    }else if (document.title === "Suite | Avatar") {
+        yMinMax = [
+            [0, 100]
+        ]
+
+        console.log("Data for Suite page loaded")
+    } else if (document.title === "VFX | Avatar") {
+        yMinMax = [
+            [0, 100]
+        ]
+        console.log("Data for VFX page loaded")
+    }
+
+
     pageYpourcent = Math.round(scrollY / (document.body.scrollHeight - innerHeight) * 100);
     tryAnimate()
     console.log(pageYpourcent)
